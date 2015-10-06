d3a744beec68fdded009b0f528a65848b82c1da2
Tyler Komoroske <tjkomor@clemson.edu>
2015-10-06 16:38:18 -0600

    I3 check guess working, base expectations met, need to work on extensions.

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,8 +2,24 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 
-number = 12
+NUMBER = rand(100)
 get '/' do
-  guess = params["guess"].to_i
-  erb :index, :locals => {:number => number}
+  guess = params["guess"]
+  message = check_guess(guess)
+  erb :index, :locals =>{:number => NUMBER, :message => message}
+end
+
+
+def check_guess(guess)
+  if (guess.to_i - NUMBER) > 20
+    "Way Too High!"
+  elsif (guess.to_i - NUMBER) < -20
+    "Way Too Low!"
+  elsif guess.to_i > NUMBER
+    "Too High!"
+  elsif guess.to_i < NUMBER
+    "Too Low!"
+  else
+    message = "The SECRET NUMBER is #{NUMBER}. You got it right!"
+  end
 end
